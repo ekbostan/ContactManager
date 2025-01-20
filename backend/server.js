89504ac09d7e201d76d7c7ee0e9e9d71b601527f
ekbostan <bostanerolkaan@gmail.com>
2025-01-20 11:18:40 -0800

    post /contacts added

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,33 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 const port = 5001;
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.use(bodyParser.json());
+app.use(cors());
+
+const contacts = []; // Db for now
+
+app.post("/contacts", (req, res) => {
+  const { name, email } = req.body;
+
+  if (!name || !email) {
+    return res.status(400).json({ message: "Name and Email are required." });
+  }
+
+  const duplicate = contacts.find((contact) => contact.email === email);
+  if (duplicate) {
+    return res
+      .status(400)
+      .json({ message: "Contact with this email already exists." });
+  }
+
+  contacts.push({ name, email });
+  res.status(201).json({ message: "Contact added successfully." });
 });
 
-
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
